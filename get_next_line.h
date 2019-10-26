fef336fe051e98a35b788fc8b1d2684a88f4fe1f
Adrien DA SILVA FERNANDES <adda-sil@e1r11p12.42.fr>
2019-10-26 16:47:35 +0200

    Fix headers protection

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,10 @@
 /*   By: adda-sil <adda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/13 17:29:54 by adda-sil          #+#    #+#             */
-/*   Updated: 2019/10/26 16:39:22 by adda-sil         ###   ########.fr       */
+/*   Updated: 2019/10/26 16:47:10 by adda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE = 1024
-#endif
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <string.h>
@@ -30,4 +27,7 @@ typedef	struct		s_fds
 	struct s_fds	*prev;
 }					t_fds;
 int					get_next_line(int fd, char **line);
+#endif
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE = 1024
 #endif
